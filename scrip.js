17c1f4cd8530cf587a94a65f407301d4ea19fac0
Ranji-moon <sararangel401@gmail.com>
2023-04-18 20:35:36 -0300

    praticando tipos e prototipos de strings e arrays

---
+++ scrip.js
@@ -0,0 +1,17 @@
+let array
+console.log('a. ', array)
+
+array = null
+console.log('b.', array)
+
+array = [3,4,5,6,7,8,9,10,11,12,13]
+console.log('c.',array.length) // 11
+
+let i = 0
+console.log('i.', array[9]) //12
+
+array[i+1] = 19
+console.log('e.', array)
+
+const valor = array[i+6]
+console.log('f.', valor)
